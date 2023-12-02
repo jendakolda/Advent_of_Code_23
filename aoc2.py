fa0a97c54dca014abf744ea3175782bb0cf320ca
jendakolda <email@jendakolda.cz>
2023-12-02 22:09:39 +0100

    Day 2, part B

--- aoc2.py
+++ aoc2.py
@@ -9,14 +9,21 @@ with open('input2.txt', 'r') as source:
         all_games[k] = tuple({j.split()[1]: int(j.split()[0]) for j in i} for i in v)
 
 key_sum = 0
+power_sum = 0
 for key, game in all_games.items():
+    valid = True
+    mins = {}
     for draw in game:
         for k, v in draw.items():
+            if v > mins.get(k, 0):
+                mins[k] = v
             if v > limits[k]:
-                break
-        else:
-            continue
-        break
-    else:
+                valid = False
+    if valid:
         key_sum += key
+    product = 1
+    for v in mins.values():
+        product = product * v
+    power_sum += product
 print('Part A: ', key_sum)
+print('Part B: ', power_sum)
